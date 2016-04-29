2ec6a0853a561cfff2b6ee4b5ef35e336dd386e7
VyacheslavHashov <vyacheslavhashov@gmail.com>
2016-04-30 01:42:38 +0300

    added raw AST

--- src/AST.hs
+++ src/AST.hs
@@ -1,44 +1,64 @@
 module AST where
 
-type Name = String
+-- Types
 
-data Type = Void | Bool | I8 | I16 | I32 | I64
-          | U8 | U16 | U32 | U64
-          deriving Show
-
-data BinOpType = OpPlus | OpMinus | OpProduct | OpDivision
-               | OpGt | OpGte | OpLt | OpLte | OpEq | OpNeq
-               | OpAnd | OpOr
-               deriving Show
-
-data UnOpType = OpNot
-   deriving Show
-
-----------------------------------
-type Program = [GlInstruction]
-
-data GlInstruction = GlVarDecl Name Type
-                   | FunDecl Name [ArgDecl] CodeBlock Type
-                   deriving Show
-
-data ArgDecl = ArgDecl Name Type
+data PrimType = TVoid | TBool | TInt | TUint | TFloat
     deriving Show
 
-type CodeBlock = [Instruction]
+data Type = ValType PrimType -- types of values
+          | FunType [PrimType] PrimType -- list of argument's types
+                                        -- and type of a returning value
 
-data Instruction = VarDecl Name Type
-                 | Assign Name Expr
-                 | Return (Maybe Expr)
-                 | IfBlock Expr CodeBlock
-                 | IfElseBlock Expr CodeBlock CodeBlock
-                 | WhileBlock Expr CodeBlock
-                 deriving Show
+-- Operators
 
-data Expr = BinOp BinOpType Expr Expr
-          | UnOp UnOpType Expr
-          | FunApply Name [Expr]
-          | Atom Int
+data BinOpArType = OpPlus | OpMinus | OpProduct | OpDivision
+    deriving Show
+
+data BinOpLogType = OpGt | OpGte | OpLt | OpLte | OpEq | OpNeq
+                  | OpAnd | OpOr
+    deriving Show
+
+data UnOpLogType = OpNot
+   deriving Show
+
+type Name = String
+
+-- Raw AST
+-- results after parsing
+
+type RawProgram = [RawGlInstruction]
+
+data RawGlInstruction = RawGlVarDecl Name PrimType
+                      | RawFunDecl Name [RawArgDecl] RawCodeBlock PrimType
+                   deriving Show
+
+data RawArgDecl = RawArgDecl Name PrimType
+    deriving Show
+
+type RawCodeBlock = [RawInstruction]
+
+data RawInstruction = RawVarDecl Name PrimType
+                    | RawAssign Name RawExpr
+                    | RawReturn (Maybe RawExpr)
+                    | RawIfBlock RawExpr RawCodeBlock
+                    | RawIfElseBlock RawExpr RawCodeBlock RawCodeBlock
+                    | RawWhileBlock RawExpr RawCodeBlock
+                    deriving Show
+
+data RawExpr = RawBinArOp BinOpArType RawExpr RawExpr
+             | RawBinLogOp BinOpLogType RawExpr RawExpr
+             | RawUnOp UnOpLogType RawExpr
+             | RawFunApply Name [RawExpr]
+             | RawLiteral String
+             | RawIdent String
           deriving Show
+
+
+-- data Value = VoidValue ()
+--            | BoolValue Bool
+--            | IntValue Int
+--            | UintValue Word64
+--            | FloatValue Double
 
 
 
